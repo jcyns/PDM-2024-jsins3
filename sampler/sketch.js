b98256d24e65e62945e90c65e9be06f44ae02ede
Cynssss <justin.sins.03@gmail.com>
2024-02-27 20:59:06 -0600

    added more samples

--- sampler/sketch.js
+++ sampler/sketch.js
@@ -1,14 +1,16 @@
-let soundFX, sample1, sample2, sample3, sample4;
+let sample1, sample2, sample3, sample4;
 let delaySlider, fbSlider, distSlider;
 
 let sounds = new Tone.Players({
     boom : "assets/boom.mp3",
-
+    click :"assets/click.mp3",
+    pikmin : "assets/pikmin.mp3",
+    malichite : "assets/malichite.mp3",
 
 }).toDestination();
 
-let delAmt = new Tone.Feedbackdelay("8n",0.5);
-let distAmt = new Tone.Distortion(0.5);
+let delAmt = new Tone.Feedbackdelay("8n",2);
+let distAmt = new Tone.Distortion(1);
 
 sounds.connect(delAmt);
 delAmt.connect(distAmt);
@@ -19,31 +21,32 @@ function setup() {
   createCanvas(400, 400);
 
   sample1 = createButton('Vine');
-  sample1.position (85,150);
+  sample1.position (65,150);
   sample1.mousePressed(() =>sounds.player ('boom').start() );
 
-  sample2 = createButton('Sample 2');
-  sample2.position(145,150);
-  sample2.mousePressed(() => sounds.player ('Sample 2').start());
+  sample2 = createButton('Click');
+  sample2.position(125,150);
+  sample2.mousePressed(() => sounds.player ('click').start());
 
-  sample3 = createButton('Sample 3');
-  sample3.position(235,150);
-  sample3.mousePressed(() => sounds.player ('Sample 3').start());
+  sample3 = createButton('Pikmin');
+  sample3.position(215,150);
+  sample3.mousePressed(() => sounds.player ('pikmin').start());
 
-  sample4 = createButton('Sample 4');
-  sample4.position(320,150);
-  sample4.mousePressed(() => sounds.player ('Sample 4').start());
+  sample4 = createButton('Malichite');
+  sample4.position(300,150);
+  sample4.mousePressed(() => sounds.player ('malichite').start());
 
-  delaySlider = createSlider (0., 0.9, 0, 0.05);
-  delaySlider.position (120,200);
+  
+  delaySlider = createSlider (0, 1, 0, 0.05);
+  delaySlider.position (120,250);
   delaySlider.mouseMoved(() => delAmt.delayTime.value = delaySlider.value());
   
-  fbSlider = createSlider (0., 0.9, 0 , 0.05);
-  fbSlider.position (120,250);
+  fbSlider = createSlider (0, 0.9, 0 , 0.05);
+  fbSlider.position (120,300);
   fbSlider.mouseMoved(() => delAmt.feedback.value = fbSlider.VALUE());
 
-  distSlider = createSlider (0., 0.9, 0 , 0.05);
-  distSlider.position (120,300);
+  distSlider = createSlider (0, 0.9, 0 , 0.05);
+  distSlider.position (120,350);
   distSlider.mouseMoved(() => distAmt.Distortion = distSlider.VALUE());
 
 }
